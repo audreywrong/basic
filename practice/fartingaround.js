0aa23a220ad113fd352613aa5f60f20e0c55ab8e
Audrey W <68037586+audreywrong@users.noreply.github.com>
2020-11-07 18:04:06 -0500

    added practice folder

---
+++ practice/fartingaround.js
@@ -0,0 +1,8 @@
+let favoriteDrink = 'Tequila';
+
+if (favoriteDrink === 'Tequila') {
+  console.log('Ole!');
+} else {
+  console.log('Well then tequila should be your favorite drink and not Flior Polish, Bundy Rum or Coors Light!');
+}
+
